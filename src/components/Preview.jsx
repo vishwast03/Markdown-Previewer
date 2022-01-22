83d9f52013a75a5d09ffd179eb8df602d68841ac
Vishwas Tyagi <vishwast8126@gmail.com>
2022-01-22 11:06:41 +0530

    fix bugs

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -5,12 +5,14 @@ const Preview = (props) => {
   useEffect(() => {
     document.getElementById("preview").innerHTML = marked.parse(props.text);
   });
-
+  
   return (
-    <div
-      className="w-1/2 h-full m-0 p-2 flex-1 overflow-auto text-base box-border border-2 border-black prose prose-lg"
-      id="preview"
-    ></div>
+    <div className="w-1/2 h-full m-0 flex-1 box-border border-2 border-black">
+      <div
+        className="w-full h-full p-2 overflow-auto text-base prose prose-lg max-w-full"
+        id="preview"
+      ></div>
+    </div>
   );
 };
 
